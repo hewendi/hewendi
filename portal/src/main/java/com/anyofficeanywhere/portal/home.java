485365c089141329871ae550c1ba2b54f7cb80b7
hewendi <627710332@qq.com>
2017-01-20 02:12:57 +0800

    add

---
+++ portal/src/main/java/com/anyofficeanywhere/portal/home.java
@@ -0,0 +1,10 @@
+package com.anyofficeanywhere.portal;
+
+public class home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ portal/src/test/java/com/anyofficeanywhere/testportal/home.java
@@ -0,0 +1,10 @@
+package com.anyofficeanywhere.testportal;
+
+public class home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
